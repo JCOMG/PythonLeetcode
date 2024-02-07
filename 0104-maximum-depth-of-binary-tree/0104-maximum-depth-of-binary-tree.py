ecb0b8315a549c3076ca0abe6330a0d873f07a8c
JCOMG <52964625+JCOMG@users.noreply.github.com>
2024-02-07 01:51:08 +0000

    Time: 49 ms (21.10%), Space: 17.7 MB (68.91%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -9,10 +9,16 @@ class Solution:
         if root is None:
             return 0
         else:
-            right_depth = self.maxDepth(root.right)
-            left_depth = self.maxDepth(root.left)
+            count_1 = 1
+            count = 1
+            if root.right is not None:
+                # 如果右子節點不為空，遞歸計算右子樹的深度
+                count += self.maxDepth(root.right)
+            if root.left is not None:
+                # 如果左子節點不為空，遞歸計算左子樹的深度
+                count_1 += self.maxDepth(root.left)
 
-            result = max(left_depth, right_depth) + 1
+            result = max(count, count_1)
             return result
         
         
