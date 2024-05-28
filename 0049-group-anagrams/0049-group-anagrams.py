7edf5f40f7ac7ade5ab2e8da5da19a663d04ab86
JCOMG <52964625+JCOMG@users.noreply.github.com>
2024-05-28 19:30:46 +0100

    Time: 85 ms (77.29%), Space: 19.5 MB (84.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dictionary = {}
-        for i in strs:
-            after_sorted = str(sorted(i))
-            
-            if after_sorted not in dictionary:
-                dictionary[after_sorted] = [i]
-                # 這裡的意思界是說 把 ['a', 'e', 't'] 變成 key , 然後值是 eat, 一定要加中括號因為要把它變成 List , 後面append 要用
-                # -> { ['a', 'e', 't'] : ['eat'] }
-            else:
-                dictionary[after_sorted].append(i)
-                # 如果有重複的話，直接append在原本的 value 的後面
-                # -> { ['a', 'e', 't'] : ['eat' , 'tea']}
-        return list(dictionary.values())
-        # return 回去 我們的 dictionary 的值 ，但是，題目要求我們也要將返回的值加個 list 
-        # 所以變成 -> [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+        dic = {}
+        for s in strs:
+            # 將字串排序後作為鍵
+            sorted_str = " ".join(sorted(s))  # 將 " " 這個空格加入sorted s 的 「後面」 
+            print(sorted_str)
+            # 如果鍵不存在，則新增一個空清單
+            if sorted_str not in dic:
+                dic[sorted_str] = []  # aet :[]
+
+            # 將字串添加到對應的鍵的清單中
+            dic[sorted_str].append(s)  # aet : ["eat",'tea', 'ate'] , ant : ['tan', 'nat'] ,abt : ['bat']
+
+        # 將字典中的值轉換為包含這些清單的清單
+        return list(dic.values())  # [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
